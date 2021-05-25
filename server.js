aa1dd79e5dfe37bcf93ffd66cbb1390a55a24d07
Aryan Malik <Aryanmalik19.am@gmail.com>
2021-05-25 22:22:18 +0530

    Updated User authentication

--- server.js
+++ server.js
@@ -4,25 +4,25 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const fs = require('fs');
-const bcrypt=require('bcrypt');
+const bcrypt = require('bcrypt');
 const dbURL = "mongodb+srv://AryanMalik:Malik2001@cluster0.xmqot.mongodb.net/food_cafe?retryWrites=true&w=majority";
 const UserModel = require('./models/userInfo');
 
 const app = express();
 
-app.engine('hbs', expHbs({ extname:'hbs'}))
+app.engine('hbs', expHbs({ extname: 'hbs' }))
 app.set('view engine', 'hbs')
-app.use(express.static(path.join(__dirname,'uploads')));
-app.use(express.urlencoded({extended: true}))
+app.use(express.static(path.join(__dirname, 'uploads')));
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(fileUpload({}))
 
 mongoose.connect(dbURL, {
     useNewUrlParser: true,
-    useUnifiedTopology: true 
-}, async(err) => {
- if (err) throw err
- console.log('Connected to database')
+    useUnifiedTopology: true
+}, async (err) => {
+    if (err) throw err
+    console.log('Connected to database')
 }
 )
 
@@ -30,15 +30,15 @@ app.get('/signup', (req, res) => {
     res.render('signup')
 })
 
-app.post('/signup',async (req, res) => {
-    const salt= await bcrypt.genSalt(10);
+app.post('/signup', async (req, res) => {
+    const salt = await bcrypt.genSalt(10);
     let myFile = req.files.avatar
-    req.body["imgUrl"]=myFile.name
-    req.body["password"]=await bcrypt.hash(req.body.password,salt);
+    req.body["imgUrl"] = myFile.name
+    req.body["password"] = await bcrypt.hash(req.body.password, salt);
     const userdata = new UserModel(req.body)
     console.log(userdata)
     await userdata.save()
-    res.send({uploaded: true})
+    res.send({ uploaded: true })
 })
 
 
@@ -46,36 +46,29 @@ app.get('/login', (req, res) => {
     res.render('login')
 })
 
-app.post('/login',async (req, res) => {
-    UserModel.find().then(data =>{
-        const password = req.body.password
-    for (let index = 0; index < data.length; index++) {
-        const dataObj = data[index];
-        console.log("password from Mongoose",dataObj.password)
-        console.log("password",password)
-        
-        console.log(bcrypt.compare("Malik2001", dataObj.password))
-        if (bcrypt.compare(password , dataObj.password) && dataObj.email == req.body.email) {
-            const expireDate = new Date('2021-05-30')
-            res.cookie('dataIdentified', dataObj.email, { expires: expireDate })
-            res.send({Login:true})
-            console.log(dataObj)
-            return
+app.post('/login', async (req, res) => {
+    const user = await UserModel.findOne({ email: req.body.email });
+    if (user) {
+        const validPassword = await bcrypt.compare(req.body.password, user.password);
+        if (validPassword) {
+            res.status(200).json({ message: "Valid password" });
+        } else {
+            res.status(400).json({ error: "Invalid Password" });
         }
+    } else {
+        res.status(401).json({ error: "User does not exist" });
     }
-    res.send({Login: false})
-    })
 })
 
-app.get('/views/Css/login.css', (req,res) => {
+app.get('/views/Css/login.css', (req, res) => {
     res.sendFile(__dirname + '/views/Css/login.css')
 })
 
-app.get('/views/Css/signup.css', (req,res) => {
+app.get('/views/Css/signup.css', (req, res) => {
     res.sendFile(__dirname + '/views/Css/signup.css')
 })
 
-app.get('/views/Css/landingPage.css', (req,res) => {
+app.get('/views/Css/landingPage.css', (req, res) => {
     res.sendFile(__dirname + '/views/Css/landingPage.css')
 })
 
